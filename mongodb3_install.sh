3fe7a1119b0480c7631be1795a42677f3f5e2074
shakasan <shakasan@sirenacorp.be>
2017-12-22 05:24:45 +0100

    minor change

--- mongodb3_install.sh
+++ mongodb3_install.sh
@@ -9,26 +9,26 @@
 ################################################################################
 
 #
-# remove the default MongoDB v2.x installed from ubuntu repository 
-# and delete all log and databases (DB from v2 and v3 are not compatible) 
+# remove the default MongoDB v2.x installed from ubuntu repository
+# and delete all log and databases (DB from v2 and v3 are not compatible)
 # !!!! so...do a backup before !!!!
 #
 sudo systemctl stop mongodb \
-&& sudo rm -rf /var/log/mongodb \
-&& sudo rm -rf /var/lib/mongodb \
-&& sudo apt remove --purge mongodb \
+  && sudo rm -rf /var/log/mongodb \
+  && sudo rm -rf /var/lib/mongodb \
+  && sudo apt remove --purge mongodb \
 
-#
+  #
 # install MongoDB Community Edition 3.x
 #
 && sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5 \
-&& echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/testing multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list \
-&& sudo apt update \
-&& sudo apt install -y mongodb-org \
+  && echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/testing multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list \
+  && sudo apt update \
+  && sudo apt install -y mongodb-org \
 
-#
+  #
 # systemd fix
 #
 && sudo systemctl unmask mongodb.service \
-&& sudo systemctl enable mongodb \
-&& sudo systemctl start mongodb
+  && sudo systemctl enable mongodb \
+  && sudo systemctl start mongodb
